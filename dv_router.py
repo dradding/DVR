88caadad6906a1a189987acd72226ef95635a135
Hamza Hashmi <hamzahashmi786@gmail.com>
2014-09-30 15:54:58 -0700

    Discovery Packet handling sort of

--- dv_router.py
+++ dv_router.py
@@ -7,8 +7,23 @@ Create your distance vector router in this file.
 class DVRouter (Entity):
     def __init__(self):
         # Add your code here!
-        pass
+        self.dv = {}
+        self.dv_neighbors = {}
+        self.neighbor_link_weights = {}
 
     def handle_rx (self, packet, port):
         # Add your code here!
-        raise NotImplementedError
+        if isinstance(packet, DiscoveryPacket):
+        	if(packet.is_link_up):
+        		available_route = RoutingUpdate()
+        		self.dv[packet.src] = packet.latency
+        		self.neighbor_link_weights[packet.src] = packet.latency
+        		available_route.paths = self.dv
+        		self.send(available_route, packet.src, flood=True)
+        elif isinstance(packet, RoutingUpdate):
+        	print "routing update"
+        	self.dv_neighbors[packet.src] = packet.paths
+        else:
+        	self.send(packet, packet.dst, flood=True)
+
+
