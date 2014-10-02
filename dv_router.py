6598e25b67b0f73c4c17c84eba9a188489df330c
Daniel Radding <dradding@gmail.com>
2014-10-01 20:01:22 -0700

    Reorganized more

--- dv_router.py
+++ dv_router.py
@@ -7,32 +7,35 @@ Create your distance vector router in this file.
 class DVRouter (Entity):
     def __init__(self):
         # Add your code here!
-        self.dv = {}
+        self.dv = {} #Key is a destination, value is next hop
         self.dv_neighbors = {}
-        self.neighbor_ports = {} #Key is neighbor, value is ports
+        self.neighbor_ports = {} #Key is neighbor, value is the port
         self.neighbor_latency = {}
 
     def handle_rx (self, packet, port):
         # Add your code here!
         if isinstance(packet, DiscoveryPacket):
-        	if(packet.is_link_up):
-        		available_route = RoutingUpdate()
-        		self.dv[packet.src] = packet.latency
-        		self.neighbor_link_weights[packet.src] = packet.latency
-        		self.neighbor_ports[packet.src] = port
-        		available_route.paths = self.dv
-        		self.send(available_route, packet.src, flood=False)
-        	#add Else
+        	handle_dp(packet, port)
         elif isinstance(packet, RoutingUpdate):
-        	print "routing update"
-        	self.dv_neighbors[packet.src] = packet.paths
+        	handle_ru(packet)
         else: #Data packet, foward approroately
         	#Look up best path in DV, send to appropriate neighbor
         	self.send(packet, packet.dst, flood=False)
 
     def handle_dp(self):
 
-    def handle_ru(self):
+    def handle_ru(self, update):
+    	ru = RoutingUpdate()
+    	latency = self.neighbor_latency[src]
+    	send_update = False
+    	for key in self.dv_neighbors[src]: #add tie breaking by lower port number
+    			if self.dv.has_key(key):
+    				if self.dv_neighbors[src][key] < self.dv_neighbors[self.dv[key]]:
+    					self.dv[key] = src
+    					send_update = True
+    					#Update RU object here
+    			else:
+    				self.dv[key] = src
 
 
     def update_dv(self, src, change_type): #src is the node that sent an updated dv
@@ -62,6 +65,7 @@ class DVRouter (Entity):
     				self.dv[key] = src
     	else:
     		pass
+
     def send_RU(self, RU):
     	for n in self.neighbor_ports:
     		self.send(RU, n, self.neighbor_ports[0])
