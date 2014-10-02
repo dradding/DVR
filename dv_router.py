4eb22c253cb3bae76b016e816151300a6492f180
Hamza Hashmi <hamzahashmi786@gmail.com>
2014-10-01 21:31:40 -0700

    works for very basic cases

--- dv_router.py
+++ dv_router.py
@@ -20,9 +20,17 @@ class DVRouter (Entity):
         	self.handle_ru(packet)
         else: #Data packet, foward approroately
         	#Look up best path in DV, send to appropriate neighbor
-        	self.send(packet, packet.dst, flood=False)
+            self.handle_data(packet)
+
+
+    def handle_data(self, packet):
+        next_hop = self.dv[packet.dst][0]
+        port_num = self.neighbor_ports[next_hop]
+
+        self.send(packet, port_num)
 
     def handle_dp(self, packet, port): #takes in a discovery packet
+        print "hello DP"
         update_to_send = RoutingUpdate()
         if (packet.is_link_up):
             self.dv[packet.src] = (packet.src, packet.latency)
@@ -30,28 +38,29 @@ class DVRouter (Entity):
             self.neighbor_latency[packet.src] = packet.latency
             update_to_send.add_destination(packet.src, packet.latency)
 
-        elif !(packet.is_link_up): #how to handle ports going down??
+        elif (packet.is_link_up == False): #how to handle ports going down??
             self.dv[packet.src] = (packet.src, float("inf"))
             self.neighbor_latency[packet.src] = float("inf")
 
             update_to_send.add_destination(packet.src, packet.latency)
-        self.sendRU(update_to_send)
+        self.send_RU(update_to_send)
 
     def handle_ru(self, packet):
+        print "hello RU"
     	ru = RoutingUpdate()
     	latency = self.neighbor_latency[packet.src]
     	send_update = False
     	destinations = packet.all_dests()
     	for dest in destinations: #add tie breaking via lower port number
 			if self.dv.has_key(dest):
-				if packet.get_distance[dest] + latency < self.dv[dest][1]:
-					self.dv[dest] = [packet.src, packet.get_distance[dest] + latency]
+				if packet.get_distance(dest) + latency < self.dv[dest][1]:
+					self.dv[dest] = [packet.src, packet.get_distance(dest) + latency]
 					send_update = True
 					ru.add_destination(dest, self.dv[dest][1])
 					#Update RU object here
 			else:
 				send_update = True
-				self.dv[dest] = [packet.src, packet.get_distance[dest] + latency]
+				self.dv[dest] = [packet.src, packet.get_distance(dest) + latency]
 				ru.add_destination(dest, self.dv[dest][1])
 
 			self.send_RU(ru)
