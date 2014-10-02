de02600d3021e5c8b2f35cace58cfd5ea90c3d43
Daniel Radding <dradding@gmail.com>
2014-10-01 20:54:54 -0700

    more updates to handle_ru

--- dv_router.py
+++ dv_router.py
@@ -45,41 +45,41 @@ class DVRouter (Entity):
     	destinations = packet.all_dests()
     	for dest in destinations: #add tie breaking via lower port number
     			if self.dv.has_key(dest):
-    				if packet. < self.dv_neighbors[self.dv[key]]:
-    					self.dv[key] = src
+    				if packet.get_distance[dest] + latency < self.dv[dest][1]:
+    					self.dv[key][1] = packet.get_distance[dest]
     					send_update = True
     					#Update RU object here
     			else:
-    				self.dv[dest] = packet.get_dest(dest) + latency
+    				self.dv[dest] = packet.get_distance(dest) + latency
 
 
-    def update_dv(self, src, change_type): #src is the node that sent an updated dv
-    	#Check to see if DV needs update. If updated, send routing update
-    	#Check for changes when latency changes
-    	#Eventually add Poison and Split Horizon
-    	send_update = False #Set to True if  Routing Update should be sent
-    	latency = self.neighbor_latency[src]
-    	if change_type == "DP": #Only called when link is lost. When link is added, handle_rx will handle it
-    		for key in self.dv:
-    			if self.dv[key] == src:
-    				for n in self.dv_neighbors: #n is a neighbor
-    					if self.dv_neighbors[n].has_key(key) #if the n contains a key then a path exists
-    						if self.dv_neighbors[n][key] < self.dv_neighbors[self.dv[key]]: 
-    							self.dv[key] = n
-    							send_update = True
-    							#Update RU object here
+    # def update_dv(self, src, change_type): #src is the node that sent an updated dv
+    # 	#Check to see if DV needs update. If updated, send routing update
+    # 	#Check for changes when latency changes
+    # 	#Eventually add Poison and Split Horizon
+    # 	send_update = False #Set to True if  Routing Update should be sent
+    # 	latency = self.neighbor_latency[src]
+    # 	if change_type == "DP": #Only called when link is lost. When link is added, handle_rx will handle it
+    # 		for key in self.dv:
+    # 			if self.dv[key] == src:
+    # 				for n in self.dv_neighbors: #n is a neighbor
+    # 					if self.dv_neighbors[n].has_key(key) #if the n contains a key then a path exists
+    # 						if self.dv_neighbors[n][key] < self.dv_neighbors[self.dv[key]]: 
+    # 							self.dv[key] = n
+    # 							send_update = True
+    # 							#Update RU object here
 
-    	elif change_type == "RU":
-    		for key in self.dv_neighbors[src]: #add tie breaking by lower port number
-    			if self.dv.has_key(key):
-    				if self.dv_neighbors[src][key] < self.dv_neighbors[self.dv[key]]:
-    					self.dv[key] = src
-    					send_update = True
-    					#Update RU object here
-    			else:
-    				self.dv[key] = src
-    	else:
-    		pass
+    # 	elif change_type == "RU":
+    # 		for key in self.dv_neighbors[src]: #add tie breaking by lower port number
+    # 			if self.dv.has_key(key):
+    # 				if self.dv_neighbors[src][key] < self.dv_neighbors[self.dv[key]]:
+    # 					self.dv[key] = src
+    # 					send_update = True
+    # 					#Update RU object here
+    # 			else:
+    # 				self.dv[key] = src
+    # 	else:
+    # 		pass
 
     def send_RU(self, RU):
     	for n in self.neighbor_ports:
