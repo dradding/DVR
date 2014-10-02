4c68445c3caa1b5ca3cb377a9be8930b1f0a0b33
Daniel Radding <dradding@gmail.com>
2014-10-01 19:14:28 -0700

    Lot's of work on update_dv

--- dv_router.py
+++ dv_router.py
@@ -9,7 +9,8 @@ class DVRouter (Entity):
         # Add your code here!
         self.dv = {}
         self.dv_neighbors = {}
-        self.neighbor_link_weights = {}
+        self.neighbor_ports = {} #Key is neighbor, value is ports
+        self.neighbor_latency = {}
 
     def handle_rx (self, packet, port):
         # Add your code here!
@@ -18,12 +19,44 @@ class DVRouter (Entity):
         		available_route = RoutingUpdate()
         		self.dv[packet.src] = packet.latency
         		self.neighbor_link_weights[packet.src] = packet.latency
+        		self.neighbor_ports[packet.src] = port
         		available_route.paths = self.dv
-        		self.send(available_route, packet.src, flood=True)
+        		self.send(available_route, packet.src, flood=False)
+        	#add Else
         elif isinstance(packet, RoutingUpdate):
         	print "routing update"
         	self.dv_neighbors[packet.src] = packet.paths
-        else:
-        	self.send(packet, packet.dst, flood=True)
+        else: #Data packet, foward approroately
+        	#Look up best path in DV, send to appropriate neighbor
+        	self.send(packet, packet.dst, flood=False)
 
+    def update_dv(self, src, change_type): #src is the node that sent an updated dv
+    	#Check to see if DV needs update. If updated, send routing update
+    	#Check for changes when latency changes
+    	#Eventually add Poison and Split Horizon
+    	send_update = False #Set to True if  Routing Update should be sent
+    	latency = self.neighbor_latency[src]
+    	if change_type == "DP": #Only called when link is lost. When link is added, handle_rx will handle it
+    		for key in self.dv:
+    			if self.dv[key] = src:
+    				for n in self.dv_neighbors: #n is a neighbor
+    					if self.dv_neighbors[n].has_key(key) #if the n contains a key then a path exists
+    						if self.dv_neighbors[n][key] < self.dv_neighbors[self.dv[key]]: 
+    							self.dv[key] = n
+    							send_update=True
+    							#Update RU object here
 
+    	elif change_type == "RU":
+    		for key in self.dv_neighbors[src]: #add tie breaking by lower port number
+    			if self.dv.has_key(key):
+    				if self.dv_neighbors[src][key] < self.dv_neighbors[self.dv[key]]:
+    					self.dv[key] = src
+    					send_update = True
+    					#Update RU object here
+    			else:
+    				self.dv[key] = src
+    	else:
+    		pass
+    def send_RU(self):
+    	for n in self.neighbor_ports:
+    		self.send(RU, n, self.neighbor_ports[0])
